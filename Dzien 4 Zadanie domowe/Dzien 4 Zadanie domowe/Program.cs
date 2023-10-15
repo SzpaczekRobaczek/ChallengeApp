afdb8f686b39f755a307aecb5eecdc18e953a07c
SzpaczekRobaczek <szpak.kacper@gmail.com>
2023-10-15 18:46:15 +0200

    Dzien 4 Zadanie domowe

--- Dzien 4 Zadanie domowe/Dzien 4 Zadanie domowe/Program.cs
+++ Dzien 4 Zadanie domowe/Dzien 4 Zadanie domowe/Program.cs
@@ -4,7 +4,7 @@ var age = 50;
 
 Console.WriteLine("Rozwiazanie");
 
-if (sex == "Female" && age > 30)
+if (sex == "Female" && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
@@ -12,7 +12,7 @@ else if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-else if (age > 18 && sex == "Male")
+else if (age < 18 && sex == "Male")
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
