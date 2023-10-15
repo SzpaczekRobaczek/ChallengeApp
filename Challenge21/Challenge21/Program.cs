2892d5b99f1f189ce460af6f9c59eacce304be34
SzpaczekRobaczek <szpak.kacper@gmail.com>
2023-10-15 20:04:26 +0200

    Dzien 4 Zadanie domowe

--- Challenge21/Challenge21/Program.cs
+++ Challenge21/Challenge21/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Witam, witam o zdrowie pytam");
+﻿var name = "Ania";
+var sex = "Female";
+var age = 50;
+
+Console.WriteLine("Rozwiazanie");
+
+if (sex == "Female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && sex == "Male")
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Przypal");
+}
