044c7ffd9befd1134de0dee4f6860806413d0707
SzpaczekRobaczek <szpak.kacper@gmail.com>
2023-10-20 21:22:15 +0200

    Dzień 6 zadanie domowe

--- Challenge21/Challenge21/Program.cs
+++ Challenge21/Challenge21/Program.cs
@@ -1,71 +1,49 @@
-﻿Console.WriteLine("Zadanie domowe dzień 5");
-int number = 4588666;
-Console.WriteLine("Liczba: " + number + " posiada:");
-string numberAsString =  number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using Challenge21;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Adam", "Kowalski", "30");
+Employee employee2 = new Employee("Monika", "Lewandowska", "25");
+Employee employee3 = new Employee("Zuzia", "Nowak", "45");
 
-foreach (char letter in letters)
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(1);
+employee1.AddScore(1);
+employee1.AddScore(6);
+
+employee2.AddScore(6);
+employee2.AddScore(6);
+employee2.AddScore(6);
+employee2.AddScore(7);
+employee2.AddScore(9);
+
+employee3.AddScore(9);
+employee3.AddScore(10);
+employee3.AddScore(6);
+employee3.AddScore(10);
+employee3.AddScore(9);
+
+var result1 = employee1.Result;
+var result2 = employee2.Result;
+var result3 = employee3.Result;
+
+
+Console.WriteLine("Zadanie domowe dzień 6");
+Console.WriteLine("Osoba z największą liczbą punktów to:");
+
+if (result1 > result2 && result1 > result3)
 {
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+    Console.WriteLine(employee1.Name + " " + employee1.Surname + " lat " + employee1.Age + " posiada: " + result1 + "pkt.");
 }
-Console.WriteLine("0, które występuje: " + counter0);
-Console.WriteLine("1, które występuje: " + counter1);
-Console.WriteLine("2, które występuje: " + counter2);
-Console.WriteLine("3, które występuje: " + counter3);
-Console.WriteLine("4, które występuje: " + counter4);
-Console.WriteLine("5, które występuje: " + counter5);
-Console.WriteLine("6, które występuje: " + counter6);
-Console.WriteLine("7, które występuje: " + counter7);
-Console.WriteLine("8, które występuje: " + counter8);
-Console.WriteLine("9, które występuje: " + counter9);
-Console.WriteLine("Panie docencie mam nadzieję, że dobrze");
+else if (result2 > result1 && result2 > result3)
+{
+    Console.WriteLine(employee2.Name + " " + employee2.Surname + " lat " + employee2.Age + " posiada: " + result2 + "pkt.");
+}
+else if (result3 > result2 && result3 > result1)
+{
+    Console.WriteLine(employee3.Name + " " + employee3.Surname + " lat " + employee3.Age + " posiada: " + result3 + "pkt.");
+}
+
+
+
+
+
