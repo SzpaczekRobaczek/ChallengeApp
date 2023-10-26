a19b6d0815c372fbad7cd063abe8013cf3b71482
SzpaczekRobaczek <szpak.kacper@gmail.com>
2023-10-26 21:41:14 +0200

    Dzien 10 Zadanie domowe

--- Challenge21/Challenge21/Employee.cs
+++ Challenge21/Challenge21/Employee.cs
@@ -16,7 +16,38 @@
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+
+        public void AddGrade(long grade)
+        {
+            float gradeLong = (float)grade;
+            this.AddGrade(gradeLong);
+        }
+
+        public void AddGrade(double grade)
+        {
+            float gradeDouble = (float)grade;
+            this.AddGrade(gradeDouble);
         }
 
         public Statistics GetStatistics()
--- Challenge21/Challenge21/Program.cs
+++ Challenge21/Challenge21/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Challenge21;
 
 var employee = new Employee("Adam", "Lewandowski");
-employee.AddGrade(2);
+employee.AddGrade(4);
+employee.AddGrade("Adam");
+employee.AddGrade("200");
 employee.AddGrade(5);
 employee.AddGrade(4);
 var statistics = employee.GetStatistics();
+
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
